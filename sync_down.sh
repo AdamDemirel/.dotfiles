eb37160e9335bd06a4af8d4ec3b1dcb9cf0504ee
Adam Demirel <adxm@msn.com>
2020-04-15 18:02:39 +1000

    Create sync_down.sh

---
+++ sync_down.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+rm -rf ~/.dotfiles && git clone git@github.com:AdamDemirel/.dotfiles.git
+
+echo "hello";
+# git clone git@github.com:AdamDemirel/.dotfiles.git
+
+# alias sync_down="cat /home/adam/.config/Code - OSS/User/extensions.txt"
