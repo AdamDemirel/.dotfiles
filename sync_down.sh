93697d27da6edbc4f1d6fa89c1777eb1df094390
Adam Demirel <adxm@msn.com>
2020-04-15 18:09:18 +1000

    sync up

--- sync_down.sh
+++ sync_down.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
 
-rm -rf ~/.dotfiles && cd ~ && git clone git@github.com:AdamDemirel/.dotfiles.git
+echo "removing old dotfiles..."
+rm -rf ~/.dotfiles
 
-echo "hello";
+echo "downloading new dotfiles..."
+cd ~ && git clone git@github.com:AdamDemirel/.dotfiles.git
+
+echo "setting up symlinks"
+ln -s ~/.zshrc ~/.dotfiles/.zshrc
+
 # git clone git@github.com:AdamDemirel/.dotfiles.git
 
 # alias sync_down="cat /home/adam/.config/Code - OSS/User/extensions.txt"
