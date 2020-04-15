ea66f290ae7f9f749d723b52f3d2d3d856f1bfa3
Adam Demirel <adxm@msn.com>
2020-04-15 18:25:24 +1000

    sync up

--- sync_down.sh
+++ sync_down.sh
@@ -8,11 +8,15 @@ cd ~ && git clone git@github.com:AdamDemirel/.dotfiles.git
 
 echo "setting up symlinks"
 rm ~/.zshrc
+rm ~/.gitconfig
+rm ~/.yaourtrc
 rm ~/.config/Code\ -\ OSS/User/settings.json
 rm ~/.config/Code\ -\ OSS/User/keybindings.json
 rm ~/.config/Code\ -\ OSS/User/extensions.txt
 
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/keybindings.json ~/.config/Code\ -\ OSS/User/keybindings.json
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.yaourtrc ~/.yaourtrc
 ln -s ~/.dotfiles/settings.json ~/.config/Code\ -\ OSS/User/settings.json
+ln -s ~/.dotfiles/keybindings.json ~/.config/Code\ -\ OSS/User/keybindings.json
 ln -s ~/.dotfiles/extensions.txt ~/.config/Code\ -\ OSS/User/extensions.txt
