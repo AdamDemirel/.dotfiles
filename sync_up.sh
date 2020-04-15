84adf07ba1b382d7ee96630df0325e7bbe9702d6
Adam Demirel <adxm@msn.com>
2020-04-15 18:02:54 +1000

    Create sync_up.sh

---
+++ sync_up.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+git add . && git commit -m "sync up"
+# git clone git@github.com:AdamDemirel/.dotfiles.git
+
+
+# alias sync_up="code --list-extensions >> /home/adam/.config/Code - OSS/User/extensions.txt"
